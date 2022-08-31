829537cabf6f336616776075e09a33aeba9534ea
JLLoppez <99202989+JLLoppez@users.noreply.github.com>
2022-08-31 04:22:13 +0200

    Commit

--- robofriends/src/main/java/com/example/robofriends/Robofriend.java
+++ robofriends/src/main/java/com/example/robofriends/Robofriend.java
@@ -1,11 +1,14 @@
 package com.example.robofriends;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
-
+@Entity
+@Table
 public class Robofriend {
 	
 	@Id
